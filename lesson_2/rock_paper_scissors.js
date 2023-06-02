ac85bb8e5b1c3e9421410f6eac55d8bc32e05403
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2023-06-02 12:47:29 -0600

    more notes

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,70 +1,71 @@
-const readline = require("readline-sync")
+const readline = require("readline-sync");
 
 function winner(user, computer) {
   if ((user === "rock" && computer === "scissors") ||
-  (user === "scissors" && computer === "paper") || 
-  (user === "paper" && computer === "rock")) {
+      (user === "scissors" && computer === "paper") || 
+      (user === "paper" && computer === "rock")) {
   prompt("You win");
   } else if ((user === 'rock' && computer === 'paper') ||
-        (user === 'paper' && computer === 'scissors') ||
-        (user === 'scissors' && computer === 'rock')) {
+             (user === 'paper' && computer === 'scissors') ||
+             (user === 'scissors' && computer === 'rock')) {
   prompt('You lose');
-  }
-}
+  };
+};
 
 function prompt(message) {
-  console.log(message)
-}
+  console.log(message);
+};
 
 
 
-OPTIONS = ["rock", "paper", "scissors"]
+OPTIONS = ["rock", "paper", "scissors"];
 
-let anotherGame = true
+let anotherGame = true;
+
 while (anotherGame) {
 
-  let userChoice = ""
-  let computerChoice = ""
+  let userChoice = "";
+  let computerChoice = "";
 
   while (computerChoice === userChoice) {
-    prompt(`Choose either: ${OPTIONS.join(", ")}`)
-    userChoice = readline.question()
+    prompt(`Choose either: ${OPTIONS.join(", ")}`);
+    userChoice = readline.question();
 
     while (!OPTIONS.includes(userChoice)) {
       prompt("That's not a valid choice");
       userChoice = readline.question();
-    }
+    };
     if (userChoice[0].toLowerCase() === "r") {
-      userChoice = "rock"
+      userChoice = "rock";
     } else if (userChoice[0].toLowerCase() === "p") {
-      userChoice = "paper"
+      userChoice = "paper";
     } else {
-      userChoice = "scissors"
-    }
+      userChoice = "scissors";
+    };
 
-    computerChoice = OPTIONS[Math.floor(Math.random()* OPTIONS.length)]
+    computerChoice = OPTIONS[Math.floor(Math.random()* OPTIONS.length)];
 
-    prompt(`You chose ${userChoice}`)
-    prompt(`The computer chose ${computerChoice}`)
+    prompt(`You chose ${userChoice}`);
+    prompt(`The computer chose ${computerChoice}`);
 
     if (computerChoice === userChoice) {
-      prompt("Draw! Choose again.")
-    } 
-  }
+      prompt("Draw! Choose again.");
+    } ;
+  };
 
-  winner(userChoice, computerChoice)
+  winner(userChoice, computerChoice);
 
   prompt("Want to play again?");
   prompt("Enter 'yes' or 'no':");
 
-  let playAgain = readline.question().toLowerCase()
+  let playAgain = readline.question().toLowerCase();
 
   while (playAgain[0] !== "n" && playAgain[0] !== "y") {
-    prompt("Please enter 'yes' or 'no'")
-    playAgain = readline.question().toLowerCase()
-  }
+    prompt("Please enter 'yes' or 'no'");
+    playAgain = readline.question().toLowerCase();
+  };
 
   if (playAgain[0] !== "y") {
-    anotherGame = false
-  }
-}
+    anotherGame = false;
+  };
+};
