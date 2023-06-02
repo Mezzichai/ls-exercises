43d053d30792dda304909786b8dc5587b5147583
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2023-06-02 14:31:43 -0600

    rps best of 5

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,5 +1,10 @@
 const readline = require("readline-sync");
 
+
+function prompt(message) {
+  console.log(message);
+};
+
 function winner(user, computer) {
   if ((user === "rock" && (computer === "scissors" || computer === "lizard")) ||
       (user === "scissors" && (computer === "paper" || computer === "lizard")) || 
@@ -8,20 +13,16 @@ function winner(user, computer) {
       (user === "lizard" && (computer === "spock" || computer === "paper"))
       ) {
   prompt("You win");
+  return "user"
   } else if ((computer === "rock" && (user === "scissors" || user === "lizard")) ||
              (computer === "scissors" && (user === "paper" || user === "lizard")) || 
              (computer === "paper" && (user === "rock" || user === "spock")) || 
              (computer === "spock" && (user === "rock" || user === "scissors")) ||
              (computer === "lizard" && (user === "spock" || user === "paper"))) {
   prompt('You lose');
+  return "computer"
   };
 };
-
-function prompt(message) {
-  console.log(message);
-};
-
-
 
 const OPTIONS = ["rock", "paper", "scissors", "spock", "lizard"];
 const AbrevOPTIONS = ["r", "p", "sc", "sp", "l"];
@@ -29,41 +30,53 @@ const AbrevOPTIONS = ["r", "p", "sc", "sp", "l"];
 let anotherGame = true;
 
 while (anotherGame) {
+  let userScore = 0;
+  let computerScore = 0;
+  while (userScore < 3 && computerScore < 3) {
+    let userChoice = "";
+    let computerChoice = "";
 
-  let userChoice = "";
-  let computerChoice = "";
+    while (computerChoice === userChoice) {
+      prompt(`Choose either: ${OPTIONS.join(", ")}`);
+      userChoice = readline.question();
 
-  while (computerChoice === userChoice) {
-    prompt(`Choose either: ${OPTIONS.join(", ")}`);
-    userChoice = readline.question();
+      while (!OPTIONS.includes(userChoice)) {
+        if (userChoice[0] === "r") {
+          userChoice = "rock"
+        } else if (userChoice[0] === "p") {
+          userChoice = "paper"
+        } else if (userChoice[0] === "s" && userChoice[1] === "p") {
+          userChoice = "spock"
+        } else if (userChoice[0] === "s" && userChoice[1] === "c") {
+          userChoice = "scissors"
+        } else {
+          prompt("That's not a valid choice");
+          userChoice = readline.question();
+        }
+      };
+      
 
-    while (!OPTIONS.includes(userChoice)) {
-      if (userChoice[0] === "r") {
-        userChoice = "rock"
-      } else if (userChoice[0] === "p") {
-        userChoice = "paper"
-      } else if (userChoice[0] === "s" && userChoice[1] === "p") {
-        userChoice = "spock"
-      } else if (userChoice[0] === "s" && userChoice[1] === "c") {
-        userChoice = "scissors"
-      } else {
-        prompt("That's not a valid choice");
-        userChoice = readline.question();
-      }
+      computerChoice = OPTIONS[Math.floor(Math.random()* OPTIONS.length)];
+
+      prompt(`You chose ${userChoice}`);
+      prompt(`The computer chose ${computerChoice}`);
+
+      if (computerChoice === userChoice) {
+        prompt("Draw! Choose again.");
+      } ;
     };
-    
-
-    computerChoice = OPTIONS[Math.floor(Math.random()* OPTIONS.length)];
-
-    prompt(`You chose ${userChoice}`);
-    prompt(`The computer chose ${computerChoice}`);
-
-    if (computerChoice === userChoice) {
-      prompt("Draw! Choose again.");
-    } ;
+    let roundWinner = winner(userChoice, computerChoice);
+    if (roundWinner === "user") {
+      userScore += 1
+      prompt(`you win round ${userScore+computerScore}`)
+    } else {
+      computerScore += 1
+      prompt(`The computer wins round ${userScore+computerScore}`)
+    }
   };
+  if (userScore > computerScore) {
 
-  winner(userChoice, computerChoice);
+  } else prompt("The computer wins")
 
   prompt("Want to play again?");
   prompt("Enter 'yes' or 'no':");
@@ -78,4 +91,5 @@ while (anotherGame) {
   if (playAgain[0] !== "y") {
     anotherGame = false;
   };
+
 };
