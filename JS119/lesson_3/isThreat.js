baf237b2a4ab77d9b77f96d7b239b2ad9a4985df
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2023-06-26 22:16:39 -0600

    finished all required features

--- JS119/lesson_3/isThreat.js
+++ JS119/lesson_3/isThreat.js
@@ -1,5 +1,8 @@
 const readline  = require("readline-sync")
 
+const PLAYER = "player"
+const COMPUTER = "computer"
+
 function prompt(msg) {
   console.log(msg)
 }
@@ -57,136 +60,108 @@ function listPositions(board) {
   })
 }
 
-function playerTurn(board) {
-  let position = []
-  let row = 0;
-  let column = 0;
-  listPositions(board)
-  while (true) {
-    while (true) {
-      row = parseInt(readline.question("Choose a row 1 - 3 that you want to place an x:"))
-      if (row < 1 || 3 < row || Number.isNaN(row)) {
-        prompt("Please enter a number between 1 and 3")
-      } else {
-        position.push(row - 1)
-        break
-      }
-    } 
-    while (true) {
-      column = parseInt(readline.question("Choose a column 1 - 3 that you want to place an x:"))
-      if (column < 1 || 3 < column || Number.isNaN(column)) {
-        prompt("Please enter a number between 1 and 3")
-      } else {
-        position.push(column - 1)
-        break
-      }
-    }
 
-    if (board[position[0]][position[1]] !== " ") {
-      prompt("Please choose an unused square")
-      position = []
-      continue
-    } else {
-      board[position[0]][position[1]] = "X"
-      break;
-    }
-  }
-}
+function findWinOrDefense(board) {
 
+  let twoInARow = winningPositions.filter(positions => {
+    let filteredForTwo = positions.filter(([row, col]) => {
+      return "XO".includes(board[row][col])
+    });
 
+    if (filteredForTwo.length < 2) return false
+    return (
+           filteredForTwo.every(([row,col]) => board[row][col] === "O") || 
+           filteredForTwo.every(([row,col]) => board[row][col] === "X")
+        )
+  })
 
-function aiTurn(board) {
-
-  let playerPositions = winningPositions.map((positions) => {
-    return positions.filter(([row, column]) => {
-      return !"XO".includes(board[row][column])
+  let takingPositions = twoInARow.filter(elem => {
+    return elem.some(([row, column]) => {
+      return board[row][column] === "O"
     })
   })
-  console.log(playerPositions)
-  let twoInARow = []
-  playerPositions.forEach(positions => {
-    if(positions.length === 1) {
-      twoInARow.push(...positions)
-    }
-  })
-console.log(twoInARow)
-  let takingPositions = twoInARow.slice().filter(elem => {
-    return 2 === elem.filter(([row, column]) => {
-      return board[row][column] === "O"
-    }).length
-  })
-  
 
-  let blockingPositions = twoInARow.slice().filter(elem => {
-    return 2 === elem.filter(([row, column]) => {
+  let blockingPositions = twoInARow.filter(elem => {
+    return elem.some(([row, column]) => {
       return board[row][column] === "X"
-    }).length
+    })
   })
-
-  // let aiPositions = winningPositions.map((positions) => {
-  //   return positions.filter(([row, column]) => {
-  //     return !"O".includes(board[row][column])
-  //   })
-  // })
-
-  // let positionsToWin = []
-  // aiPositions.forEach(positions => {
-  //   if (positions.length === 1) {
-  //     positionsToWin.push(...positions)
-  //   }
-  // })
-
+//using the .find higher order method would have been simpler
   if (0 < takingPositions.length) {
-    console.log(takingPositions)
-    return takingPositions
+    return takingPositions.shift().filter(elem => {
+      return board[elem[0]][elem[1]] === " "
+    })
   } else if (0 < blockingPositions.length) {
-    // console.log(blockingPositions)
-    return blockingPositions
+    return blockingPositions.shift().filter(elem => {
+      return board[elem[0]][elem[1]] === " "
+    })
   }
-
   return false
 }
-
-
-// function positionToWin(board) {
-//   let aiPositions = winningPositions.map((positions) => {
-//     return positions.filter(([row, column]) => {
-//       return !"O".includes(board[row][column])
-//     })
-//   })
-
-// }
-
 
 // //minimax algorithm 
 // takes 3 parameters, the board, the depth, and a bool indicating whether
 // the player is maximizing or minmizing the score
 
 
-
-
-
-
-
 function computerTurn(board) {
   let position = []
-  while (true) {
-    if (!aiTurn(board)) {
-      position.push(Math.floor(Math.random() * 3), Math.floor(Math.random() * 3))
-    } else {   
-      let positionsToBlock = aiTurn(board)
-      let blockingPosition = positionsToBlock[Math.floor(Math.random()*positionsToBlock.length)]
-      position.push(blockingPosition[0], blockingPosition[1])
-    }
-    if (board[position[0]][position[1]] !== " ") {
-      position = []
-      continue
-    } else {
+
+    if (findWinOrDefense(board)) {
+      let positionToTake = findWinOrDefense(board)
+      console.log(positionToTake)
+      let takingPosition = positionToTake[Math.floor(Math.random() * positionToTake.length)]
+      position.push(takingPosition[0], takingPosition[1])
       board[position[0]][position[1]] = "O"
-      break
+    } else if (board[1][1] === " ") {   
+      board[1][1] = "O"
+    } else {
+      let available = availablePositions(board)
+      let spaces = available[Math.floor(Math.random() * available.length)]
+      position.push(...spaces)
+
+      board[position[0]][position[1]] = "O"
+    }    
+  }
+
+  function playerTurn(board) {
+    let position = []
+    let row = 0;
+    let column = 0;
+    listPositions(board)
+    while (true) {
+      while (true) {
+        row = parseInt(readline.question("Choose a row 1 - 3 that you want to place an x:"))
+        if (row < 1 || 3 < row || Number.isNaN(row)) {
+          prompt("Please enter a number between 1 and 3")
+        } else {
+          position.push(row - 1)
+          break
+        }
+      } 
+      while (true) {
+        column = parseInt(readline.question("Choose a column 1 - 3 that you want to place an x:"))
+        if (column < 1 || 3 < column || Number.isNaN(column)) {
+          prompt("Please enter a number between 1 and 3")
+        } else {
+          position.push(column - 1)
+          break
+        }
+      }
+  
+      if (board[position[0]][position[1]] !== " ") {
+        prompt("Please choose an unused square")
+        position = []
+        continue
+      } else {
+        board[position[0]][position[1]] = "X"
+        break;
+      }
     }
   }
-}
+
+  
+
 
 
 function determineWin(player) {
@@ -202,6 +177,8 @@ function determineWin(player) {
   });
 }
 
+
+
 function isWinning(board) {
 
   let playerWon = determineWin("player")
@@ -214,9 +191,13 @@ function isWinning(board) {
   }
 }
 
+
+
 function isTie(board) {
   return !board.some(row => row.includes(" "))
 }
+
+
 
 function clearBoard() {
   board = [
@@ -233,44 +214,60 @@ while (true) {
   prompt("Welcome to Tic Tac Toe!")
   let playerScore = 0;
   let compScore = 0;
+
+  let whoseTurn = readline.question('Who will go first? \n computer or player or random? ')
+  if (whoseTurn === "random") {
+    let choice = Math.floor(Math.random() * 2)
+    if (choice === 1) {
+      whoseTurn = PLAYER
+    } else {
+      whoseTurn = COMPUTER
+    }
+  }
   while (playerScore + compScore < 5) {
     prompt(`Game ${playerScore + compScore} / 5`)
 
-    playerTurn(board)
+   
 
-    if (isWinning(board) === "player"){
-      prompt("You won!")
-      playerScore += 1
+    if (whoseTurn === PLAYER) {
+      playerTurn(board)
+
+      if (isWinning(board) === PLAYER){
+        prompt("You won!")
+        playerScore += 1
+        displayBoard(board)
+        clearBoard()
+        continue
+      } else if (isTie(board)) {
+        prompt("Tie!")
+        displayBoard(board)
+        clearBoard()
+        continue
+      } else {
+        prompt("Your move:")
+      }
       displayBoard(board)
-      clearBoard()
-      continue
-    } else if (isTie(board)) {
-      prompt("Tie!")
-      displayBoard(board)
-      clearBoard()
-      continue
+      whoseTurn = COMPUTER
     } else {
-      prompt("Your move:")
-    }
-    displayBoard(board)
+      computerTurn(board)
 
-    computerTurn(board)
-
-    if (isWinning(board) === "computer"){
-      prompt("You lose!")
-      compScore += 1
+      if (isWinning(board) === "computer"){
+        prompt("You lose!")
+        compScore += 1
+        displayBoard(board)
+        clearBoard()
+        continue
+      } else if (isTie(board)) {
+        prompt("Tie!")
+        displayBoard(board)
+        clearBoard()
+        continue
+      } else {
+        prompt("The computers move:")
+      }
       displayBoard(board)
-      clearBoard()
-      continue
-    } else if (isTie(board)) {
-      prompt("Tie!")
-      displayBoard(board)
-      clearBoard()
-      continue
-    } else {
-      prompt("The computers move:")
+      whoseTurn = PLAYER
     }
-    displayBoard(board)
   }
 
   if (playerScore < compScore) {
@@ -280,24 +277,3 @@ while (true) {
   let answer = readline.question("Want to play again?")
   if (answer[0] !== "y") break;
 }
-
-
-
-
-
-
-
-// problem: 
-//   detect when a player has won a game of tictactoe
-
-//   All three squares within any row are marked with the player's marker.
-//   All three squares within any column are marked with the player's marker.
-//   All three diagonal squares in either direction are marked with the player's marker.
-
-
-//   Rewriting these rules in terms of our implementation, we can say 
-//   that a player wins when any of the following sequences of squares
-//    all contain the player's marker:
-
- 
-
